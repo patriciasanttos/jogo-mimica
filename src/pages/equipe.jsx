9c67daaa92e42f73d020916d87bc4aef88335344
Patricia Santos <patriciasanttos.dev@gmail.com>
2024-11-25 18:40:05 -0300

    Funções de adicionar, remover e enter criados

--- src/pages/equipe.jsx
+++ src/pages/equipe.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Header from "../components/Header/Header";
@@ -5,10 +6,40 @@ import IconeEquipe from "../assets/icone-equipe.svg";
 import IconeAdd from "../assets/icone-add.svg";
 
 function Equipe() {
+  const [listaEquipe, setListaEquipe] = useState([]);
+  const [textoEquipe, setTextoEquipe] = useState("");
+
   const navigate = useNavigate();
 
   const onClickButton = () => {
     navigate("/categorias");
+  };
+
+  const adicionarEquipe = () => {
+    if (listaEquipe.length < 6) {
+      setListaEquipe([...listaEquipe, textoEquipe]);
+      setTextoEquipe("");
+    } else {
+      alert("Número máximo de equipes");
+    }
+  };
+
+  const onKeyDownTextoEquipe = (e) => {
+    if (e.key === "Enter") {
+      adicionarEquipe();
+    }
+  };
+
+  const onChangeTextoEquipe = (e) => {
+    setTextoEquipe(e.target.value);
+  };
+
+  const removerEquipe = (equipeParaRemover) => {
+    setListaEquipe(
+      listaEquipe.filter((equipe) => {
+        return equipe !== equipeParaRemover;
+      })
+    );
   };
 
   return (
@@ -19,13 +50,22 @@ function Equipe() {
           <input
             className="input-list"
             type="text"
-            name=""
             placeholder="Digite sua equipe"
+            onChange={onChangeTextoEquipe}
+            onKeyDown={onKeyDownTextoEquipe}
+            value={textoEquipe}
           />
         </div>
-        <div className="img-add">
+        <div className="img-add" onClick={adicionarEquipe}>
           <img src={IconeAdd} alt="" />
         </div>
+      </div>
+      <div className="lista">
+        <ul>
+          {listaEquipe.map((equipe) => {
+            return <li onClick={() => removerEquipe(equipe)}>{equipe}</li>;
+          })}
+        </ul>
       </div>
       <div className="btn-container">
         <div className="btn-next">
--- src/pages/individual.jsx
+++ src/pages/individual.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Header from "../components/Header/Header";
@@ -5,11 +6,33 @@ import IconeIndividual from "../assets/icone-individual.svg";
 import IconeAdd from "../assets/icone-add.svg";
 
 function Individual() {
+  const [listaIndividual, setListaIndividual] = useState([])
+  const [textoIndividual, setTextoIndividual] = useState("")
+  
   const navigate = useNavigate();
 
   const onClickButton = () => {
     navigate("/categorias");
   };
+
+  const adicionarIndividual = () => {
+    if(listaIndividual.length < 10) {
+      setListaIndividual([...listaIndividual, textoIndividual]);
+      setTextoIndividual("")
+    } else {
+      alert('Número máximo de pessoas')
+    }
+  }
+
+const removerIndividual = (individualParaRemover) => {
+  listaIndividual.filter((individual) => {
+    return individual !== individualParaRemover
+  })
+}
+
+const onChangeTextoIndividual = (e) => {
+  setTextoIndividual(e.target.value)
+}
 
   return (
     <>
@@ -19,13 +42,21 @@ function Individual() {
           <input
             className="input-list"
             type="text"
-            name=""
             placeholder="Digite o seu nome"
+            onChange={onChangeTextoIndividual}
+            value={textoIndividual}
           />
         </div>
-        <div className="img-add">
+        <div className="img-add" onClick={adicionarIndividual}>
           <img src={IconeAdd} alt="" />
         </div>
+      </div>
+      <div className="lista">
+        <ul>
+          {listaIndividual.map((individual) => {
+            return <li onClick={() => removerIndividual(individual)}>{individual}</li>
+          })}
+        </ul>
       </div>
       <div className="btn-container">
         <div className="btn-next">
